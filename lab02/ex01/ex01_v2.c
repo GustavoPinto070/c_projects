0106fc16c81075200642179f12db2a4795cb8582
Gustavo <gustavo.c.pinto@tecnico.ulisboa.pt>
2026-02-24 11:59:36 +0000

    ex01

---
+++ lab02/ex01/ex01_v2.c
@@ -0,0 +1,9 @@
+/* iaed26 - ist1118070 - lab02/ex01 */
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n", (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c));
+    return 0;
+}
